f33d00969db629420b68514ba1d344d7adfe9a22
unknown <vladi00345@gmail.com>
2019-11-29 11:48:04 -0800

    Added sidebar styling

--- public/Edit/index.php
+++ public/Edit/index.php
@@ -3,7 +3,6 @@
   require_once "../../src/Initializer.php";
   initialize();
   $article = new \Blog\Database\Article();
-  require_once "../partials/PastArticles.php";
 
   if (isset($_POST['contribution'])){
     $title = isset($_POST['title']) ? $_POST['title'] : null;
@@ -13,7 +12,11 @@
   $article->getLatestArticle(function($articleData){
     global $article;
     $contributions = $article->getContributions($articleData['id']);
-    $edit = new \Blog\Template\Template('edit.html.twig', ['contributions'=>$contributions, 'article'=>$articleData]);
+    $pastArticles = $article->getPastArticles();
+    $edit = new \Blog\Template\Template('edit.html.twig', [
+      'contributions'=>$contributions,
+      'article'=>$articleData,
+      'pastArticles'=>$pastArticles]);
     $edit->output();
   });
   $article->closeConnection();
--- public/partials/PastArticles.php
+++
@@ -1,8 +0,0 @@
-<?php
-  namespace Blog;
-
-  $article->getPastArticles(function($articles){
-    $sidebar = new \Blog\Template\Template("partials/sidebar.html.twig", ['articles'=>$articles]);
-    $sidebar->output();
-  });
-?>
