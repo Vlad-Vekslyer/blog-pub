f51046b3d0707d51a0c0dd78e9d3c2bb8ce4fbf5
unknown <vladi00345@gmail.com>
2019-11-29 22:45:05 -0800

    Implemented basic text area manipulation logic

--- public/Edit/index.js
+++ public/Edit/index.js
@@ -6,3 +6,20 @@ textarea.addEventListener("input", () => {
   if(textarea.value != "") button.disabled = false;
   else button.disabled = true;
 });
+
+textarea.addEventListener("keydown", function(event) {
+  if(event.key == "Enter"){
+    event.preventDefault();
+    let form = this.parentElement;
+    let newTextarea = document.createElement("TEXTAREA");
+    newTextarea.classList.add('contribution');
+    form.appendChild(newTextarea);
+  } else {
+    // calculated with element width divided by text.length when the text area is at full capacity
+    const fontWidth = 7.9;
+    const maxSize = Math.ceil(this.getBoundingClientRect().width / fontWidth);
+    console.log(this.textLength % maxSize);
+    if(this.textLength % maxSize == 0 && event.key != "Backspace") this.style.height = `40px`;
+    else if(this.textLength % maxSize == 1 && event.key == "Backspace") this.style.height = "20px";
+  }
+});
