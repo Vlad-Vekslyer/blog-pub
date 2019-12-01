739b3857708429671c4b810fb8b794ffe86a797a
unknown <vladi00345@gmail.com>
2019-12-01 13:51:11 -0800

    Optimization and bugfixing

--- public/process.php
+++ public/process.php
@@ -15,10 +15,10 @@
     "contribution-3" => "This is a **bold** contribution"
   );
 
-  foreach ($contributions as $key => $value) {
+  foreach ($contributions as $key => &$value) {
     $firstChars = substr($value, 0, 2);
     if(in_array($firstChars, $oneliners)){
-      processOneliner($firstChars, $value) . "<br/>";
+      processOneliner($firstChars, $value);
       continue;
     }
     foreach($multiliners as $multiliner){
@@ -26,27 +26,30 @@
       $count = 1;
       do {
         $pos = strpos($value, $multiliner, $pos + 2);
-        if($count++ % 2 == 0)
-          echo processMultiliner($multiliner, $value) . "<br/>";
+        if($count++ % 2 == 0 && $pos !== false){
+          processMultiliner($multiliner, $value);
+        }
       } while($pos !== false);
     }
   }
 
-  function processOneliner($oneliner, $contribution){
+  function processOneliner($oneliner, &$contribution){
     global $linerDictionary;
     $tags = $linerDictionary[$oneliner];
-    $newContribution = str_replace($oneliner, $tags[0], $contribution);
-    $newContribution .= $tags[1];
-    return $newContribution;
+    $contribution = substr_replace($contribution, $tags[0], 0, 2);
+    $contribution .= $tags[1];
   }
 
-  function processMultiliner($multiliner, $contribution){
+  function processMultiliner($multiliner, &$contribution){
     global $linerDictionary;
     $tags = $linerDictionary[$multiliner];
     $pos = strpos($contribution, $multiliner, 0);
-    $newContribution = substr_replace($contribution, $tags[0], $pos, 2);
-    $pos = strpos($newContribution, $multiliner, 0);
-    $newContribution = substr_replace($newContribution, $tags[1], $pos, 2);
-    return $newContribution;
+    $contribution = substr_replace($contribution, $tags[0], $pos, 2);
+    $pos = strpos($contribution, $multiliner, 0);
+    $contribution = substr_replace($contribution, $tags[1], $pos, 2);
   }
+
+  echo $contributions["contribution-1"] . "<br/>";
+  echo $contributions["contribution-2"] . "<br/>";
+  echo $contributions["contribution-3"] . "<br/>";
 ?>
