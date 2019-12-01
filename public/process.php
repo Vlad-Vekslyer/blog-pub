d55b4c88d4ab9f3c5798eaf22b5387bdff56309c
unknown <vladi00345@gmail.com>
2019-12-01 12:56:39 -0800

    Implemented string replacement logic

--- public/process.php
+++ public/process.php
@@ -3,6 +3,12 @@
   $oneliners = array("##");
   $multiliners = array("%%", "**");
 
+  $linerDictionary = array(
+    "##" => array("<h1>", "</h1>"),
+    "%%" => array("<em>", "</em>"),
+    "**" => array("<b>", "</b>")
+  );
+
   $contributions = array(
     "contribution-1" => "##Hello this is a header contribution",
     "contribution-2" => "This is an %%emphasis%% contribution",
@@ -12,7 +18,7 @@
   foreach ($contributions as $key => $value) {
     $firstChars = substr($value, 0, 2);
     if(in_array($firstChars, $oneliners)){
-      echo "I found a header inside of $key <br/>";
+      processOneliner($firstChars, $value) . "<br/>";
       continue;
     }
     foreach($multiliners as $multiliner){
@@ -21,8 +27,26 @@
       do {
         $pos = strpos($value, $multiliner, $pos + 2);
         if($count++ % 2 == 0)
-          echo "I found a multiliner inside of $key <br/>";
+          echo processMultiliner($multiliner, $value) . "<br/>";
       } while($pos !== false);
     }
+  }
+
+  function processOneliner($oneliner, $contribution){
+    global $linerDictionary;
+    $tags = $linerDictionary[$oneliner];
+    $newContribution = str_replace($oneliner, $tags[0], $contribution);
+    $newContribution .= $tags[1];
+    return $newContribution;
+  }
+
+  function processMultiliner($multiliner, $contribution){
+    global $linerDictionary;
+    $tags = $linerDictionary[$multiliner];
+    $pos = strpos($contribution, $multiliner, 0);
+    $newContribution = substr_replace($contribution, $tags[0], $pos, 2);
+    $pos = strpos($newContribution, $multiliner, 0);
+    $newContribution = substr_replace($newContribution, $tags[1], $pos, 2);
+    return $newContribution;
   }
 ?>
