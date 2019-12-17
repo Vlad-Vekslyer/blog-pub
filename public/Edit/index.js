be4ac7b7e2ef9c1a75b836548eb8c27022c9fb43
unknown <vladi00345@gmail.com>
2019-12-16 22:53:27 -0800

    Added comments

--- public/Edit/index.js
+++ public/Edit/index.js
@@ -6,6 +6,7 @@ const reviewSection = document.getElementById("review");
 
 reviewButton.addEventListener("click", () => {
   let inputs = document.querySelectorAll("textarea.contribution");
+  // will store the values of all contributions to send to server
   let inputValues = [];
   inputs.forEach(contribution => inputValues.push(contribution.value));
   let body = {
@@ -22,12 +23,14 @@ reviewButton.addEventListener("click", () => {
   })
   .then(res => res.json())
   .then(res => {
+    // clear out the review section from any divs
     for(let i = 0; i < reviewSection.children.length; i++) {
         if(reviewSection.children[i].tagName == "DIV") {
           reviewSection.children[i].remove();
           i--;
         }
     }
+    // populate the review section with the data received from server
     res.contributions.forEach(contribution => {
       let section = document.createElement("DIV");
       section.innerHTML = contribution;
