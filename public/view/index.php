a3230fa675633fac688dd057601155f188084bcc
unknown <vladi00345@gmail.com>
2019-12-19 00:42:09 -0800

    Added a route to view past articles

--- public/view/index.php
+++ public/view/index.php
@@ -4,11 +4,18 @@
   initialize();
   $article = new \Blog\Database\Article();
 
-  $article->getLatestArticle(function($articleData) {
-    global $article;
-    $contributions = $article->getContributions($articleData['id']);
+
+  if(isset($_GET['id'])){
+    $contributions = $article->getContributions($_GET['id']);
     $view = new \Blog\Template\Template('view.html.twig', ['contributions' => $contributions]);
     $view->output();
-  });
+  } else {
+    $article->getLatestArticle(function($articleData) {
+      global $article;
+      $contributions = $article->getContributions($articleData['id']);
+      $view = new \Blog\Template\Template('view.html.twig', ['contributions' => $contributions]);
+      $view->output();
+    });
+  }
   $article->closeConnection();
 ?>
