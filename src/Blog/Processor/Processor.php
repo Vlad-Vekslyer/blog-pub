2fc3a8a7350eab87a575c5d221f676b44a67257b
unknown <vladi00345@gmail.com>
2019-12-19 15:54:30 -0800

    Documented processor.php

--- src/Blog/Processor/Processor.php
+++ src/Blog/Processor/Processor.php
@@ -10,22 +10,27 @@
     static $oneliners = array("##");
     static $multiliners = array("%%", "**");
 
+    // turns custom syntax tags into HTML markup
     public static function processContributions(&$contributions){
       foreach ($contributions as &$value) {
         $value = \htmlspecialchars($value, ENT_QUOTES);
+        // check if the tag is a one-liner by looking at the first two characters
         $firstChars = substr($value, 0, 2);
         if(in_array($firstChars, self::$oneliners)){
           self::processOneliner($firstChars, $value);
           continue;
         }
+        // find the multiliner tags
         foreach(self::$multiliners as $multiliner){
           $pos = -2;
           $count = 1;
           do {
+            // find position of the multiliner tags starting from 0
             $pos = strpos($value, $multiliner, $pos + 2);
+            // only process if both an opening and a closing multiliner tag are found
             if($count++ % 2 == 0 && $pos !== false)
               self::processMultiliner($multiliner, $value);
-          } while($pos !== false);
+          } while($pos !== false); // loop stops when no more multiliner tags can be found
         }
       }
     }
