1f04d70894ea80a2201bf4acdfd3a03b34f93ad2
unknown <vladi00345@gmail.com>
2019-12-19 15:43:26 -0800

    Switched to a p tag instead of textareas

--- public/Edit/Contribution.js
+++ public/Edit/Contribution.js
@@ -1,4 +1,3 @@
-const fontWidth = 7.9;
 let contributionNum = 1;
 
 class Contribution {
@@ -7,16 +6,16 @@ class Contribution {
   constructor() {
     if (contributionNum < 5){
       let contributions = document.querySelector(".contributions");
-      let newTextarea = document.createElement("TEXTAREA");
-      newTextarea.classList.add("contribution");
-      newTextarea.setAttribute("name", `contribution-${contributionNum++}`);
-      this.contribution = newTextarea;
+      let newParagraph = document.createElement("P");
+      newParagraph.classList.add("contribution");
+      newParagraph.setAttribute("name", `contribution-${contributionNum++}`);
+      newParagraph.setAttribute("contenteditable", 'true');
+      this.contribution = newParagraph;
       this.buttonToggleEvent();
       this.onSelection();
-      this.sizeAdjustEvent();
       this.createContributionEvent();
-      contributions.appendChild(newTextarea);
-      newTextarea.focus();
+      contributions.appendChild(newParagraph);
+      newParagraph.focus();
     } else {
       alert("Reached maximum number of contributions");
     }
@@ -38,25 +37,6 @@ class Contribution {
     this.contribution.addEventListener("input", () => {
       if(this.contribution.value != "") button.disabled = false;
       else button.disabled = true;
-    });
-  }
-
-  // controls the size of a textarea to adjust to the number of characters written
-  sizeAdjustEvent(){
-    this.contribution.addEventListener("keydown", function(event) {
-        // calculated with element width divided by text.length when the text area is at full capacity
-        const maxSize = Math.ceil(this.getBoundingClientRect().width / fontWidth);
-        const textareaHeight = parseInt(getComputedStyle(this).height);
-        if(this.textLength % maxSize == 0 && event.key != "Backspace" && this.textLength != 0) {
-          this.style.height = `${textareaHeight + 20}px`;
-        }
-        else if(this.textLength % maxSize == 1 && event.key == "Backspace" && textareaHeight != 20) {
-          this.style.height = `${textareaHeight - 20}px`;
-        }
-    });
-
-    this.contribution.addEventListener("keyup", function() {
-      if(this.textLength == 0) this.style.height = "20px";
     });
   }
 
--- public/Edit/decorate.js
+++ public/Edit/decorate.js
@@ -42,6 +42,28 @@ function manipulate(charList, decoration, selections){
   }
 }
 
+
+function processText(decoratedCont){
+  let body = {
+    contributions : [decoratedCont]
+  }
+  body = JSON.stringify(body);
+  return fetch("/edit/process", {
+    method: 'POST',
+    mode: "same-origin",
+    body: body,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(res => res.json())
+  .then(res => {
+    return res.contributions[0];
+  })
+  .catch(error => console.error("Fetch error:" + error.message))
+
+}
+
 // initialize the decoration event listeners
 function initDecorator(){
   let buttons = document.getElementById('decorator').children;
@@ -52,16 +74,16 @@ function initDecorator(){
       let oneLinerKeys = Object.keys(oneLiners);
       // if clicked on a one-liner decoratorion, selections is null
       if (oneLinerKeys.indexOf(this.name) !== -1) selections = null;
-      else selections = {start: selectedCont.selectionStart, end: selectedCont.selectionEnd};
+      else selections = {start: window.getSelection().getRangeAt(0).startOffset, end: window.getSelection().getRangeAt(0).endOffset};
       // pass in the current this to allow the decorate function to know which button was clicked
-      let decoratedCont = decorate.call(this, selectedCont.value, selections);
+      let decoratedCont = decorate.call(this, selectedCont.innerText, selections);
       // fire an enter event to create a new textarea if a one-liner was added
-      if(decoratedCont.length > selectedCont.value.length && !selections) {
+      if(decoratedCont.length > selectedCont.innerText.length && !selections) {
           let enterEvent = new KeyboardEvent("keydown", {key: "Enter"});
           selectedCont.dispatchEvent(enterEvent);
       }
-      selectedCont.value = decoratedCont;
-    })
+      processText(decoratedCont).then(processedCont => selectedCont.innerHTML = processedCont);
+    });
   }
 }
 
