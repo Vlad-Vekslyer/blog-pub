cece83581653532262ff5806677f534287ec34ae
unknown <vladi00345@gmail.com>
2020-01-16 16:46:12 -0800

    Added flash messages styling

--- public/pages/index.php
+++ public/pages/index.php
@@ -2,6 +2,15 @@
   namespace Blog;
   require_once "../../src/Initializer.php";
   initialize();
+
+  // flash messages testing block
+  session_start();
+  $_SESSION['flash'] = [
+      'code' => 'success',
+      'msg' => 'Successfully logged in'
+  ];
+  //
+
   $landing = new \Blog\Template\Template('landing.html.twig', [
     'flash' => isset($_SESSION['flash']) ? $_SESSION['flash'] : NULL,
     'username' => isset($_SESSION['username']) ? $_SESSION['username'] : NULL
