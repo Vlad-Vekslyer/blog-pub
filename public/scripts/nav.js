c9b755d17094318c916589b5122bfb128ab2472f
unknown <vladi00345@gmail.com>
2020-01-23 14:22:49 -0800

    Styled the back and login/register buttons in the mobile nav-forms. Added logic to return to mobile menu

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -1,11 +1,18 @@
+// GENERIC INITIALIZATION
 const flashClose = document.getElementById('close-btn');
-const loginBtn = document.querySelector('nav #login-btn');
-const registerBtn = document.querySelector('nav #register-btn');
-const loginBtnMobile = document.querySelector('#nav-footer #login-btn');
-const registerBtnMobile = document.querySelector('#nav-footer #register-btn');
 const loginForm = document.getElementById('login-form');
 const registerForm = document.getElementById('register-form');
+
+// MOBILE INITIALIZATION
 const hamburger = document.getElementById('hamburger');
+const loginBtnMobile = document.querySelector('#nav-footer #login-btn');
+const registerBtnMobile = document.querySelector('#nav-footer #register-btn');
+const loginBackMobile = document.querySelector('#login-form .mobile-btn:nth-of-type(2)');
+const registerBackMobile = document.querySelector("#register-form .mobile-btn:nth-of-type(2)");
+
+// DESKTOP INITIALIZATION
+const loginBtn = document.querySelector('nav #login-btn');
+const registerBtn = document.querySelector('nav #register-btn');
 
 if(flashClose){
   flashClose.addEventListener('click', function() {
@@ -14,6 +21,9 @@ if(flashClose){
     grandparent.removeChild(parent);
   })
 }
+
+loginBackMobile.addEventListener('click', formBack('login'));
+registerBackMobile.addEventListener('click', formBack('register'));
 
 hamburger.addEventListener('click', () => document.querySelector('#nav-footer .menu').classList.toggle('drop'));
 
@@ -24,17 +34,24 @@ registerBtnMobile.addEventListener('click', formReveal('register', true));
 loginForm.addEventListener('submit', submission('login'));
 registerForm.addEventListener('submit', submission('register'));
 
+function formBack(type) {
+  return function(event) {
+    event.preventDefault();
+    document.getElementById('bottom').classList.remove('slide');
+    document.getElementById(`${type}-form`).classList.add('hidden');
+  }
+}
+
 // higher order function that will handle form revealing after user clicks login/register on nav
 function formReveal(type, isMobile = false) {
   return function(){
-    if(isMobile === true) document.getElementById('bottom').classList.toggle('slide');
+    if(isMobile === true) document.getElementById('bottom').classList.add('slide');
     else {
       const secondButton = type === 'login' ? registerBtn : loginBtn;
       secondButton.classList.remove('current');
       this.classList.toggle('current');
     }
     const form = document.getElementById(`${type}-form`);
-    console.log(form);
     const prevForm = type === 'login' ? registerForm : loginForm;
     form.classList.toggle('hidden');
     prevForm.classList.add('hidden');
